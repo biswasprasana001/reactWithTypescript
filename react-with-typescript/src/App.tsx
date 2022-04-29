faf045e9e429ff9b491975df4739b52c486b198e
biswasprasana001 <biswasprasana004@gmail.com>
2022-04-29 16:38:50 +0530

    Finished the Basic project in TypeScript

--- react-with-typescript/src/App.tsx
+++ react-with-typescript/src/App.tsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import List from './components/List';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import List from "./components/List";
+import AddToList from "./components/AddToList";
 
-interface IState {
+export interface IState {
   people: {
-    name: string,
-    age: number,
-    url: string,
-    note?: string // It's optional
-  }[]
+    name: string;
+    age: number;
+    url: string;
+    note?: string; // It's optional
+  }[];
 }
 
 function App() {
-  const [people, setPeople] = useState<IState["people"]>([])
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      age: 36,
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      note: "Allergic to staying on the same team",
+    },
+  ]);
 
   return (
     <div className="App">
       <h1>people invited to my party</h1>
       <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
--- react-with-typescript/src/components/List.tsx
+++ react-with-typescript/src/components/List.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-
-interface IProps {
-    people: {
-        name: string,
-        age: number,
-        url: string,
-        note?: string // It's optional
-    }[]
-}
+import React from "react";
+import { IState as IProps } from "../App";
 
 const List: React.FC<IProps> = ({ people }) => {
-    return (
-        <div>
-            I am a List
-        </div>
-    )
-}
+  const renderList = () => {
+    return people.map((person) => {
+      return (
+        <li className="List">
+          <div className="List-header">
+            <img className="List-img" src={person.url} />
+            <h2>{person.name}</h2>
+          </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+  };
+  return <ul>{renderList()}</ul>;
+};
 
-export default List
+export default List;
